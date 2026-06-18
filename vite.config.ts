a49d6a35a88aa9f42c5ba4fe6c18f76c49e8fd21
Cuebitt <136763482+cuebitt@users.noreply.github.com>
2026-06-18 13:49:15 -0700

    Initialize project

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite-plus";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+
+// https://vite.dev/config/
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
